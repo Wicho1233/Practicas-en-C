1c5f1d10eda53d2dc5a440e42ecd8820d94c5be3
Wicho1233 <castroluisalberto984@gmail.com>
2025-02-13 23:03:16 -0600

    Primera practica en c

---
+++ Practica1.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main() {
+ printf("Hola Mundo \r\n"); //mensaje con salto de linea
+ printf("Numero %d \r\n",10+2+3);//Especificador de numero
+ printf("Suma  %d + %d = %d \r\n",2,3,2+3);//%d especifica dos numeors al principio por ultimo una suma
+ 
+
+}
