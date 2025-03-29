a5e0eeb07e1ff1d7413a1136cfad6a27d25af0b5
Luis Alberto Castro Cruz <157860624+Wicho1233@users.noreply.github.com>
2025-03-28 21:34:24 -0600

    Update Practica1.c

--- Practica1.c
+++ Practica1.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main() {
- printf("Hola Mundo \r\n"); //mensaje con salto de linea
+ printf("Hola Mundo \r\n"); //mensaje con salto de line
  printf("Numero %d \r\n",10+2+3);//Especificador de numero
  printf("Suma  %d + %d = %d \r\n",2,3,2+3);//%d especifica dos numeors al principio por ultimo una suma
  
